dd895c7d895a3d91e964c32468c0e253fe44e2d8
Eranosyan Vova <eranosyanvova@gmail.com>
2021-11-07 21:10:39 +0300

    Completed second of four stages of contact project

--- Contacts/task/src/contacts/Person.java
+++ Contacts/task/src/contacts/Person.java
@@ -1,43 +1,38 @@
 package contacts;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Person {
-
+abstract class PhoneBookRecord {
     private String name;
-    private String surname;
-    private String number = "";
+    private String number;
+    private LocalDateTime createdTime;
+    private LocalDateTime lastEditTime;
+    private boolean isPeople;
 
     protected String getName() {
         return name;
     }
 
-    protected String getSurname() {
-        return surname;
-    }
-
-    protected String getNumber() {
-        return number;
-    }
-
-    protected Person setName(String name) {
+    protected PhoneBookRecord setName(String name) {
         this.name = name;
         return this;
     }
 
-    protected Person setSurname(String surname) {
-        this.surname = surname;
-        return this;
+    public String getNumber() {
+        return hasNumber() ? "[no number]" : number;
     }
 
-    protected Person setNumber(String number) {
+    protected PhoneBookRecord setNumber(String number) {
         if (checkNumber(number)) {
             this.number = number;
         }
         else {
             System.out.println("Wrong number format!");
-            this.number = "";
+            this.number = null;
         }
         return this;
     }
@@ -49,16 +44,121 @@ public class Person {
     }
 
     protected boolean hasNumber() {
-        return !number.isEmpty();
+        return number == null;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime.withSecond(0).withNano(0);
+    }
+
+    public void setCreatedTime() {
+        createdTime = LocalDateTime.now();
+        lastEditTime = createdTime;
+    }
+
+    public LocalDateTime getLastEditTime() {
+        return lastEditTime.withSecond(0).withNano(0);
+    }
+
+    public void setLastEditTime() {
+        lastEditTime = LocalDateTime.now();
+    }
+
+    public boolean isPeople() {
+        return isPeople;
+    }
+
+    public void setIsPeople(boolean isPeople) {
+        this.isPeople = isPeople;
+    }
+
+}
+
+
+public class Person extends PhoneBookRecord{
+    private String surname;
+    private String gender;
+    private LocalDate birthDay;
+
+    public Person() {
+        setCreatedTime();
+        setIsPeople(true);
+    }
+
+    protected String getSurname() {
+        return surname;
+    }
+
+    protected Person setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public String getGender() {
+        return gender == null ? "[no data]" : gender;
+    }
+
+    public Person setGender(String gender) {
+        if (gender.equalsIgnoreCase("M") || gender.equalsIgnoreCase("F")) {
+            this.gender = gender;
+        } else {
+            System.out.println("Bad gender!");
+        }
+        return this;
+    }
+
+    public String getBirthDay() {
+        return birthDay == null ? "[no data]" : birthDay.toString();
+    }
+
+    public Person setBirthDay(String birthDay) {
+        try {
+            this.birthDay = LocalDate.parse(birthDay);
+        } catch (DateTimeParseException e) {
+            System.out.println("Bad birth date!");
+        }
+        return this;
+    }
+
+    protected String getFullName() {
+        return getName() + " " + getSurname();
     }
 
     @Override
     public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", firstname='" + surname + '\'' +
-                ", number='" + number + '\'' +
-                '}';
+        return "Name: " + getName() +
+                "\nSurname: " + getSurname() +
+                "\nBirth date: " + getBirthDay() +
+                "\nGender: " + getGender() +
+                "\nNumber: " + getNumber() +
+                "\nTime created: " + getCreatedTime() +
+                "\nTime last edit: " + getLastEditTime();
+    }
+}
+
+class Organization extends PhoneBookRecord {
+    private String address;
+
+    public Organization() {
+        setCreatedTime();
+        setIsPeople(false);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Organization name: " + getName() +
+                "\nAddress: " + getAddress() +
+                "\nNumber: " + getNumber() +
+                "\nTime created: " + getCreatedTime() +
+                "\nTime last edit: " + getLastEditTime();
     }
 }
 
--- Contacts/task/src/contacts/PhoneBook.java
+++ Contacts/task/src/contacts/PhoneBook.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 
 public class PhoneBook {
     Scanner scanner = new Scanner(System.in);
-    private ArrayList<Person> phoneBook;
+    private ArrayList<PhoneBookRecord> phoneBook;
 
     public PhoneBook() {
-        this.phoneBook = new ArrayList<>();
+        phoneBook = new ArrayList<>();
     }
 
     public void menu() {
         String choose;
         do {
-            System.out.print("Enter action (add, remove, edit, count, list, exit): ");
+            System.out.print("Enter action (add, remove, edit, count, info, exit): ");
             choose = scanner.nextLine();
             switch (choose) {
                 case "add":
-                    addPersonInPhoneBook();
+                    addRecordInPhoneBook();
                     break;
                 case "remove":
                     remove();
@@ -29,33 +29,50 @@ public class PhoneBook {
                 case "count":
                     System.out.printf("The Phone Book has %d records.\n", phoneBook.size());
                     break;
-                case "list":
-                    list();
+                case "info":
+                    infoAboutRecord();
                     break;
                 case "exit":
                     System.exit(0);
                     break;
                 default:
             }
+            System.out.println();
         } while (true);
     }
 
-
-    protected void addPersonInPhoneBook() {
-        System.out.print("Enter the name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter the surname: ");
-        String surname = scanner.nextLine();
-        System.out.print("Enter the number: ");
-        String number = scanner.nextLine();
-        phoneBook.add(new Person().setName(name).setSurname(surname).setNumber(number));
+    protected void addRecordInPhoneBook() {
+        String choose;
+        System.out.print("Enter the type (person, organization): ");
+        choose = scanner.nextLine();
+        CreateRecord createRecord;
+        if ("person".equals(choose)) {
+            createRecord = new AddPersonInPhoneBook();
+        }
+        else if ("organization".equals(choose)) {
+            createRecord = new AddOrganizationInPhoneBook();
+        }
+        else {
+            System.out.println("Wrong choose: " + choose);
+            return;
+        }
+        phoneBook.add(createRecord.creatRecord());
         System.out.println("The record added.");
+    }
+
+    protected void infoAboutRecord() {
+        if (!phoneBook.isEmpty()) {
+            list();
+            System.out.print("Enter index to show info: ");
+            System.out.println(phoneBook.get(Integer.parseInt(scanner.nextLine()) - 1));
+        } else {
+            System.out.println("No records to see info!");
+        }
     }
 
     protected void list() {
         for (int i = 0; i < phoneBook.size(); i++) {
-            System.out.printf("%d. %s %s, %s\n", i + 1, phoneBook.get(i).getName(), phoneBook.get(i).getSurname(),
-                    phoneBook.get(i).hasNumber() ? phoneBook.get(i).getNumber() : "[no number]");
+            System.out.printf("%d. %s \n", i + 1, phoneBook.get(i).isPeople() ? ((Person)phoneBook.get(i)).getFullName() : phoneBook.get(i).getName());
         }
     }
 
@@ -78,22 +95,13 @@ public class PhoneBook {
             System.out.print("Select thee records: ");
             int choose = scanner.nextInt() - 1;
             scanner.nextLine();
-            System.out.print("Select a field (name, surname, number): ");
-            String lineToEdit = scanner.nextLine();
-            switch (lineToEdit) {
-                case "name":
-                    System.out.print("Enter name: ");
-                    phoneBook.get(choose).setName(scanner.nextLine());
-                    break;
-                case "surname":
-                    System.out.print("Enter surname: ");
-                    phoneBook.get(choose).setSurname(scanner.nextLine());
-                    break;
-                case "number":
-                    System.out.print("Enter number: ");
-                    phoneBook.get(choose).setNumber(scanner.nextLine());
-                    break;
+            EditRecord editRecord;
+            if (phoneBook.get(choose).isPeople()) {
+                editRecord = new EditPersonInPhoneBook(phoneBook.get(choose));
+            } else {
+                editRecord = new EditOrganizationInPhoneBook(phoneBook.get(choose));
             }
+            editRecord.editRecord();
             System.out.println("The record updated!");
         }
         else {
@@ -102,4 +110,125 @@ public class PhoneBook {
 
     }
 
+}
+
+
+interface EditRecord {
+    Scanner scanner = new Scanner(System.in);
+    void editRecord();
+}
+
+class EditPersonInPhoneBook implements EditRecord {
+    Person person;
+
+    public EditPersonInPhoneBook(PhoneBookRecord phoneBookRecord) {
+        person = (Person) phoneBookRecord;
+    }
+
+    @Override
+    public void editRecord() {
+        System.out.print("Select a field (name, surname, birth, gender, number): ");
+        String lineToEdit = scanner.nextLine();
+        switch (lineToEdit) {
+            case "name":
+                System.out.print("Enter the name: ");
+                person.setName(scanner.nextLine());
+                break;
+            case "surname":
+                System.out.print("Enter the surname: ");
+                person.setSurname(scanner.nextLine());
+                break;
+            case "birth":
+                System.out.print("Enter the birth date: ");
+                person.setBirthDay(scanner.nextLine());
+                break;
+            case "gender":
+                System.out.print("Enter the gender (M, F): ");
+                person.setGender(scanner.nextLine());
+                break;
+            case "number":
+                System.out.print("Enter number: ");
+                person.setNumber(scanner.nextLine());
+                break;
+            default:
+                System.out.println("Wrong choose: " + lineToEdit);
+        }
+        person.setLastEditTime();
+    }
+}
+
+class EditOrganizationInPhoneBook implements EditRecord {
+    Organization organization;
+
+    public EditOrganizationInPhoneBook(PhoneBookRecord phoneBookRecord) {
+        organization = (Organization) phoneBookRecord;
+    }
+
+    @Override
+    public void editRecord() {
+        System.out.print("Select a field (name, address, number): ");
+        String lineToEdit = scanner.nextLine();
+        switch (lineToEdit) {
+            case "name":
+                System.out.print("Enter the name: ");
+                organization.setName(scanner.nextLine());
+                break;
+            case "address":
+                System.out.print("Enter the surname: ");
+                organization.setAddress(scanner.nextLine());
+                break;
+            case "number":
+                System.out.print("Enter number: ");
+                organization.setNumber(scanner.nextLine());
+                break;
+            default:
+                System.out.println("Wrong choose: " + lineToEdit);
+        }
+        organization.setLastEditTime();
+    }
+}
+
+
+
+
+
+
+
+interface CreateRecord {
+    Scanner scanner = new Scanner(System.in);
+    PhoneBookRecord creatRecord();
+}
+
+class AddPersonInPhoneBook implements CreateRecord {
+
+    @Override
+    public PhoneBookRecord creatRecord() {
+        Person person = new Person();
+        System.out.print("Enter the name: ");
+        person.setName(scanner.nextLine());
+        System.out.print("Enter the surname: ");
+        person.setSurname(scanner.nextLine());
+        System.out.print("Enter the birth date: ");
+        person.setBirthDay(scanner.nextLine());
+        System.out.print("Enter the gender: ");
+        person.setGender(scanner.nextLine());
+        System.out.print("Enter the number: ");
+        person.setNumber(scanner.nextLine());
+        return person;
+    }
+}
+
+class AddOrganizationInPhoneBook implements CreateRecord {
+
+    @Override
+    public PhoneBookRecord creatRecord() {
+        Organization organization = new Organization();
+        System.out.print("Enter the organization name: ");
+        organization.setName(scanner.nextLine());
+        System.out.print("Enter the address: ");
+        organization.setAddress(scanner.nextLine());
+        System.out.print("Enter the number: ");
+        organization.setNumber(scanner.nextLine());
+        return organization;
+    }
 }
